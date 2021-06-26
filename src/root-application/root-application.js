51621afc1eb2f8d0ecf79dc6afc7a8c26905626e
nitin reddy <reddynitin3@gmail.com>
2021-06-26 10:22:23 +0530

    Updated the angular version to 9

--- src/root-application/root-application.js
+++ src/root-application/root-application.js
@@ -1,14 +1,14 @@
 import * as singleSpa from 'single-spa';
 
 singleSpa.registerApplication('app-1', () =>
-  import ('../app1/app1.js'), pathPrefix('/app1'));
+  import('../react/app1.js'), pathPrefix('/react'));
 singleSpa.registerApplication('app-2', () =>
-  import ('../app2/app2.js'), pathPrefix('/app2'));
+  import('../angular/app2.js'), pathPrefix('/angular'));
 
 singleSpa.start();
 
 function pathPrefix(prefix) {
-  return function(location) {
+  return function (location) {
     return location.pathname.startsWith(`${prefix}`);
   }
 }
